d1b9c8bebe999d658421c77ae3104ecb049b380d
wskraymond <starry2011night@gmail.com>
2021-11-12 17:49:18 +0800

    update

---
+++ src/main/java/com/mine/twoheaps/MedianFinder.java
@@ -0,0 +1,22 @@
+package com.mine.twoheaps;
+
+public class MedianFinder {
+    public MedianFinder() {
+
+    }
+
+    public void addNum(int num) {
+
+    }
+
+    public double findMedian() {
+
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
