bdaa61b9d290a15bf09f5dca82d37df0d4d06e26
wskraymond <starry2011night@gmail.com>
2021-12-14 23:25:14 +0800

    update

--- src/main/java/com/mine/graph/dfs/AllPaths_NON_DAG.java
+++ src/main/java/com/mine/graph/dfs/AllPaths_NON_DAG.java
@@ -24,6 +24,22 @@ public class AllPaths_NON_DAG {
     }
 
     private void dfs(int s, int d, int[][] graph , Set<Integer> visit, List<Integer> path, List<List<Integer>> result){
+        /**
+         * https://leetcode.com/discuss/general-discussion/136503/what-is-difference-between-backtracking-and-depth-first-search
+         *
+         * Difference 1:
+         * DFS handles an explicit tree.While Backtracking handles an implicit tree
+         *
+         * Difference 2:
+         * Depth First Search is a special type of backtracking algorithmic design paradigm
+         * where the process of backtracking takes place in the leaf nodes.
+         * In case of backtracking,the algorithm also rejects the useless branch of the state-space tree.
+         * This is why DFS maintains the entire tree structure while Backtracking maintaines a pruned tree
+         *
+         * https://stackoverflow.com/questions/1294720/whats-the-difference-between-backtracking-and-depth-first-search/17891070
+         *
+         *
+         */
         if(s==d){
             result.add(new LinkedList<>(path)); //clone
             return;
