84c1d3d7ccffe31762bba2ebeea0e715c811e9a7
wskraymond <starry2011night@gmail.com>
2023-04-09 17:05:50 +0800

    upd;ate

--- src/main/java/com/mine/Intervals/NonOverlapIntervals.java
+++ src/main/java/com/mine/Intervals/NonOverlapIntervals.java
@@ -1,8 +1,13 @@
 package com.mine.Intervals;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class NonOverlapIntervals {
     /**
-     * Given an array of intervals intervals where intervals[i] = [starti, endi], return the minimum number of intervals you need to remove to make the rest of the intervals non-overlapping.
+     * Given an array of intervals intervals where intervals[i] = [starti, endi],
+     * return the minimum number of intervals you need
+     * to remove to make the rest of the intervals non-overlapping.
      *
      *
      *
@@ -33,6 +38,18 @@ public class NonOverlapIntervals {
      * @return
      */
     public int eraseOverlapIntervals(int[][] intervals) {
-        return 0;
+        int count = 0;
+        Arrays.sort(intervals, Comparator.comparingInt(interval->interval[0]));
+        int end = intervals[0][1];
+        for(int i=1;i<intervals.length;i++){
+            if(end > intervals[i][0]){
+                end = Math.min(end , intervals[i][1]);
+                count++;
+            } else {
+                end = intervals[i][1];
+            }
+        }
+
+        return count;
     }
 }
